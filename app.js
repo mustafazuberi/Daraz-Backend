3d5f88da648c2edbbdb1db6c4da7e42ba1855586
Mustafa Zuberi <mustafazuberi986@gmail.com>
2023-02-15 15:58:59 +0500

    origin url changed

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(express.json({ limit: '50mb' }))
 
 app.use(bodyParser.json());
 const corsOptions = {
-    origin: 'https://daraz-clone-mr-zuberi.netlify.app/',
+    origin: 'https://daraz-clone-mr-zuberi.netlify.app',
     credentials: true,            //access-control-allow-credentials:true
     optionSuccessStatus: 200
 }
